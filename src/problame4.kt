3e6b7a9d2aafd5568a993457f44d610e2d4761c7
CGM-23 <cludiogonsales@gmail.com>
2025-09-09 14:41:04 -0500

    Add files via upload

---
+++ src/problame4.kt
@@ -0,0 +1,9 @@
+fun main() {
+	val distancia: Double = 400.0
+    val trenA: Double = 80.0
+    val trenB: Double = 70.0
+    var tiempoEncuentro: Double
+    
+    tiempoEncuentro = distancia / (trenA + trenB)
+    println("Los trenes se encontraran en $tiempoEncuentro segundos")
+}
