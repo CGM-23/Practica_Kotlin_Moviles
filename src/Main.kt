2f986355bfe8177fc50b27c9a7b3a5caf813eb17
CGM-23 <cludiogonsales@gmail.com>
2025-09-09 14:30:54 -0500

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,16 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    print("Digite la cantidad de numeros: ")
+
+    var n = scanner.nextInt()
+
+    while ( n<= 0) {
+        println("Digito un numero invalido, intenta otra vez")
+        print("->")
+        n = scanner.nextInt()
+    }
+    var suma=n*(n+1)/2
+    print("La suma de "+n+" numeros es " +suma)
+}
